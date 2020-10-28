c5788909bb71c106673b88f5d8e61e9370aa48f9
JalanJiang <448300947@qq.com>
2020-10-28 17:24:45 +0800

    feat(page): 添加话题列表

--- pages/topics/topics.js
+++ pages/topics/topics.js
@@ -1,3 +1,30 @@
+const app = getApp()
+
 Page({
-  
+  data: {
+    total: 0,
+    topicList: [],
+  },
+  onLoad: function (query) {
+    console.log(query.subscription_id)
+    this.featchTopics(query.subscription_id)
+  },
+  onPullDownRefresh: function() {
+
+  },
+  featchTopics: function (subscriptionID, page = app.globalData.defaultPage, pageSize = app.globalData.defaultPageSize) {
+    wx.request({
+      url: app.globalData.baseUrl + '/api/v1/topics?subscription_id=' + subscriptionID + '&page=' + page + '&page_size=' + pageSize,
+      method: 'GET',
+      header: {
+        'Authorization': wx.getStorageSync('token')
+      },
+      success: (res) => {
+        this.setData({
+          'total': res.data.total,
+          'topicList': res.data.topics
+        })
+      }
+    })
+  }
 })
